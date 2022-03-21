39cd2ac0ae48567249d7ec05b523a0332e4351fa
Maxim Kolchurin <maxim.kolchurin@gmail.com>
2022-03-21 13:55:08 +0300

    connect string fix

--- newtmgr/config/mqtt_config.go
+++ newtmgr/config/mqtt_config.go
@@ -7,19 +7,22 @@ import (
 	"strings"
 )
 
+// connstring = "DeviceID, BrokerAddr, UserName, Password, QoS, ClientID".
+// For example:
+// connstring="Client1,tcp://localhost:1883,,,0,0" or
+// connstring="Client1,tcp://localhost:1883,User,Password,0,1"
 func ParseMqttConnString(cs string) (*nmqtt.MqttXPortCfg, error) {
 	sc := nmqtt.NewXportCfg()
 
 	parts := strings.Split(cs, ",")
 
 	sc.Id = parts[0]
-
 	sc.Broker = parts[1]
 	sc.User = parts[2]
 	sc.Password = parts[3]
-	sc.DeviceId, _ = strconv.Atoi(parts[4])
-	i, _ := strconv.Atoi(parts[5])
-	sc.Qos = int8(i)
+	qos, _ := strconv.Atoi(parts[4])
+	sc.Qos = int8(qos)
+	sc.DeviceId, _ = strconv.Atoi(parts[5])
 
 	return sc, nil
 }
