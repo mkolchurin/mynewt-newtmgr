4ad2f23fdc3dfd9fbfc8dce246c0ddfd382da87e
Maxim Kolchurin <maxim.kolchurin@gmail.com>
2022-04-02 12:21:41 +0300

    check mqtt connstring errors

--- newtmgr/config/mqtt_config.go
+++ newtmgr/config/mqtt_config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"errors"
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"mynewt.apache.org/newt/util"
 	"mynewt.apache.org/newtmgr/nmxact/nmqtt"
@@ -16,18 +18,26 @@ func ParseMqttConnString(cs string) (*nmqtt.MqttXPortCfg, error) {
 	sc := nmqtt.NewXportCfg()
 
 	parts := strings.Split(cs, ",")
-
+	strlen := len(parts)
+	if strlen < 6 {
+		return nil, errors.New("wrong args format")
+	}
 	sc.Id = parts[0]
 	sc.Broker = parts[1]
 	sc.User = parts[2]
 	sc.Password = parts[3]
-	qos, _ := strconv.Atoi(parts[4])
+	qos, e := strconv.Atoi(parts[4])
+	if e != nil {
+		return nil, fmt.Errorf("failed to parse QOS with error '%s'", e.Error())
+	}
 	if qos != 0 && qos != 1 && qos != 2 {
-		log.Errorf("Failed to set QOS %d", qos)
+		return nil, fmt.Errorf("QOS '%d' is not valid", qos)
 	}
 	sc.Qos = int8(qos)
-	sc.DeviceId, _ = strconv.Atoi(parts[5])
-
+	sc.DeviceId, e = strconv.Atoi(parts[5])
+	if e != nil {
+		return nil, fmt.Errorf("failed to parse device id with error '%s'", e.Error())
+	}
 	log.Infof("mqtt id '%s'; Broker '%s'; user '%s'; qos '%d'; devId '%d'", sc.Id, sc.Broker, sc.User, sc.Qos, sc.DeviceId)
 	return sc, nil
 }
